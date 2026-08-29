c52682442ab93ac7ecbfc3cbf69c8d793028b4ea
atapardea <atapardea@gmail.com>
2026-08-29 18:48:30 +0300

    added providers

--- infrastructure/hyperv/providers.tf
+++ infrastructure/hyperv/providers.tf
@@ -1,1 +1,12 @@
-# Terraform and Hyper-V provider configuration will be introduced in the next step.
+terraform {
+  required_providers {
+    hyperv = {
+      source  = "windsorcli/hyperv"
+      version = "0.4.0"
+    }
+  }
+}
+
+provider "hyperv" {
+  # Configuration options will be added only if the local Hyper-V host requires them.
+}
