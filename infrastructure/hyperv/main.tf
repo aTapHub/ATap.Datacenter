8ff9b1cda093828d4d0167bf6fb205afacb70007
atapardea <atapardea@gmail.com>
2026-08-30 12:02:24 +0300

    added vhdx resource

--- infrastructure/hyperv/main.tf
+++ infrastructure/hyperv/main.tf
@@ -10,3 +10,9 @@ resource "hyperv_vm" "control_plane" {
     startup_bytes = 4 * 1024 * 1024 * 1024
   }
 }
+
+resource "hyperv_vhd" "control_plane" {
+  path       = "D:/Homelab/virtual-disks/k8s-cp-01.vhdx"
+  size_bytes = 40 * 1024 * 1024 * 1024 # 40 GiB
+  vhd_type   = "dynamic"
+}
