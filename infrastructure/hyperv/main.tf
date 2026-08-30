fea720f14e23908512612f085ba28a5ee294fa0a
atapardea <atapardea@gmail.com>
2026-08-30 12:08:43 +0300

    added vhdx to vm

--- infrastructure/hyperv/main.tf
+++ infrastructure/hyperv/main.tf
@@ -9,6 +9,15 @@ resource "hyperv_vm" "control_plane" {
   memory = {
     startup_bytes = 4 * 1024 * 1024 * 1024
   }
+
+  hard_disk_drive = [
+    {
+      path                = hyperv_vhd.control_plane.path
+      controller_type     = "SCSI"
+      controller_number   = 0
+      controller_location = 0
+    }
+  ]
 }
 
 resource "hyperv_vhd" "control_plane" {
